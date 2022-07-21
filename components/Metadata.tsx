67effc53e456c38088bf2896908d1a4dc3c62ef1
Enzo <enzovivallo05@outlook.com>
2022-07-20 23:34:12 -0400

    fix: metadata url value

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import isProduction from 'utils/isProduction'
 
 interface MetadataProps {
   title?: string
@@ -9,23 +8,18 @@ interface MetadataProps {
 const Metadata = ({
   title,
   description = 'Share your thoughts with the world.',
-}: MetadataProps) => {
-  const protocol = isProduction ? 'https' : 'http'
-  const url = `${protocol}://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-
-  return (
-    <Head>
-      <title>{title ? `${title} | Tweeter` : 'Tweeter'}</title>
-      <meta name="description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-    </Head>
-  )
-}
+}: MetadataProps) => (
+  <Head>
+    <title>{title ? `${title} | Tweeter` : 'Tweeter'}</title>
+    <meta name="description" content={description} />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="twitter:url" content={process.env.NEXT_PUBLIC_URL} />
+    <meta property="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+  </Head>
+)
 
 export default Metadata
