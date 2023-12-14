155e339fff6c38ee3ba64d959d46f38811065809
Enzo <47456460+MasterKrab@users.noreply.github.com>
2023-12-14 02:43:15 -0300

    fix: image domain protocol

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['api.dicebear.com', 'res.cloudinary.com', 'secure.gravatar.com', 'avatars.githubusercontent.com'],
+    remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "api.dicebear.com",
+        },
+      ],
   },
 }
 
