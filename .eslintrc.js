f21a0d179e26d84dd6fe262b7f892a766fec7db7
Enzo Vivallo <enzovivallo@estudiante.uc.cl>
2025-06-21 00:19:02 -0400

    fix: typing errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,27 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:react/recommended',
-    "next/core-web-vitals",
+    'next/core-web-vitals',
     'standard',
-    "prettier"
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     '@next/next/no-img-element': 'off',
     'import/no-anonymous-default-export': 'off',
-    "no-undef": "off"
-  }
+    'no-undef': 'off',
+    'react/no-unknown-property': 'off',
+  },
 }
